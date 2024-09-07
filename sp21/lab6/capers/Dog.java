bfd4258ab68f3726556540fbba4b73b274de6271
Mortis <471917017@qq.com>
2024-09-07 09:38:43 +0800

    finished lab6

--- sp21/lab6/capers/Dog.java
+++ sp21/lab6/capers/Dog.java
@@ -1,5 +1,7 @@
 package capers;
 
+import org.junit.Test;
+
 import java.io.File;
 import java.io.Serializable;
 import static capers.Utils.*;
@@ -7,10 +9,10 @@ import static capers.Utils.*;
 /** Represents a dog that can be serialized.
  * @author TODO
 */
-public class Dog { // TODO
+public class Dog implements Serializable { // TODO
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
+    static final File DOG_FOLDER = Utils.join(CapersRepository.CAPERS_FOLDER,"dogs"); // TODO (hint: look at the `join`
                                          //      function in Utils)
 
     /** Age of dog. */
@@ -40,7 +42,8 @@ public class Dog { // TODO
      */
     public static Dog fromFile(String name) {
         // TODO (hint: look at the Utils file)
-        return null;
+        File dog = Utils.join(DOG_FOLDER, name);
+        return Utils.readObject(dog, Dog.class);
     }
 
     /**
@@ -57,6 +60,8 @@ public class Dog { // TODO
      */
     public void saveDog() {
         // TODO (hint: don't forget dog names are unique)
+        File dog = Utils.join(DOG_FOLDER, name);
+        Utils.writeObject(dog, this);
     }
 
     @Override
